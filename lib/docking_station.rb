ee9a52f16c5753b60fa44c8f2c52cb43854b59b9
Darrenf <darrenf54321@gmail.com>
2016-09-27 16:49:36 +0100

    stuck on chap 11git add . Ahhhh

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,15 @@
-require_relative 'bike'
+
 class DockingStation
   def release_bike
     Bike.new
   end
+
+  #def dock(bike)
+    #@bike = bike #Darren
+  #end
+
+  #def bike #Darren
+    #@bike
+  #end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,23 @@
 require "docking_station"
 
-describe DockingStation do
- it { is_expected.to respond_to :release_bike}
- it "should get a working bike" do
-   bike = subject.release_bike
-   expect(bike).to be_working
- end
+  describe DockingStation do
+
+   it { is_expected.to respond_to :release_bike}
+
+   it "should get a working bike" do
+     bike = subject.release_bike
+     expect(bike).to be_working
+   end
+
+    it { is_expected.to respond_to(:bike) }
+
+    it { is_expected.to respond_to(:dock).with(1).argument }
+
+#Darren
+    #it 'returns docked bikes' do
+    #bike = Bike.new
+    #subject.dock(bike)
+    #expect(subject.bike).to eq bike
+    #end
+
 end
